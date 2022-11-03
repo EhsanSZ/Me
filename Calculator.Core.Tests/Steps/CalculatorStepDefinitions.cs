6ecc06743b51f016651142e81ba205c993c0d0ae
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-11-03 14:20:53 +0330

    Specflow1

---
+++ Calculator.Core.Tests/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace Calculator.Core.Tests.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly Calculator _calculator = new Calculator();
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _scenarioContext["add_result"] = _calculator.Add();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            var addResult = int.Parse(_scenarioContext.GetValueOrDefault("add_result").ToString());
+            addResult.Should().Be(result);
+        }
+    }
+}
